0caacac6551a98ffd7251bcf8978cc2e4372bae2
messeili <mymoa.2014@gmail.com>
2021-01-14 00:55:05 +0200

    prcticing vuejs

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,49 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+<h1>Todo App</h1>
+<form @submit.prevent="addNewTodo">
+  <label for="name">New Task</label>
+  <input v-model="newTodo" type="text" name="todo" required>
+  <button>Add new Task</button>
+</form>
+  <button @click="markAllDone">Mark All As Done</button>
+  <button @click="deleteAll">Delete all</button>
+<ul>
+<li v-for="task in tasks" :key="task.id">
+<h3 :class="{done:task.done}" @click="toggleDone(task)">{{task.content}}</h3>
+</li>
+</ul>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import {ref} from 'vue'
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+setup(){
+  const newTodo = ref('')
+  const tasks = ref([])
+const addNewTodo = ()=>{
+  if(newTodo.value){
+    tasks.value.push({
+      id: Date.now(),
+      done:false,
+      content: newTodo.value
+    });
   }
+    newTodo.value=''
+}
+
+const toggleDone = (task)=>{
+  task.done = !task.done
+}
+const markAllDone=()=>{
+tasks.value.forEach(item=>{
+item.done=true
+})
+}
+const deleteAll = ()=>{
+  tasks.value.length=0
+}
+return{addNewTodo,newTodo,tasks,toggleDone,markAllDone,deleteAll}
+}
 }
 </script>
 
@@ -22,5 +55,9 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+.done{
+  text-decoration: line-through;
 }
 </style>
